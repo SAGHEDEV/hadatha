ec14779f32753de202d37b5c6a88d19b8461f025
SAGHEDEV <abdulhakeem080255@gmail.com>
2025-12-13 22:20:14 +0100

    fix: basic event display issue

--- hadatha_frontend/app/layout.tsx
+++ hadatha_frontend/app/layout.tsx
@@ -5,8 +5,29 @@ import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "Hadatha",
-  description: "Hadatha - The Ultimate Ticketing Platform built on Sui",
-};
+  description: "Create, discover, and attend events on Sui.",
+  openGraph: {
+    title: "Event ticketing Platform on Sui",
+    description: "Create, discover, and attend events on Sui.",
+    url: "https://hadathaio.vercel.app/",
+    siteName: "Hadatha",
+    images: [
+      {
+        url: "https://hadathaio.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "YourApp Preview",
+      },
+    ],
+    type: "website",
+  },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "My Web3 Event Platform",
+  //   description: "Create, discover, and attend events on Sui.",
+  //   images: ["https://yourdomain.com/og-image.png"],
+  // },
+}
 
 export default function RootLayout({
   children,
